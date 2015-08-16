9d6e10486ac54952d7543ab36093ef5806855d8b
Sven Mkw <sven.mkw@gmail.com>
2015-08-16 17:25:32 +0200

    * added overlay for groundplan * added golfplatz image on index * adapted footer * adapted config/ impress data * added favicon

--- src/javascripts/lib/smoothScroll.js
+++ src/javascripts/lib/smoothScroll.js
@@ -1,24 +1,37 @@
 $(function() {
-  $('a[data-scroll=true][href*=#]:not([href=#])').click(function(evt) {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      var time = 2000;
-      var customTime = $(evt.currentTarget).attr("data-scroll-time");
+  $('[data-scroll=true]')
+    .filter('*[href*=#]:not([href=#]), *[data-target]')
+    .click(function(evt) {
+      var hasPathname = this.pathname !== undefined;
 
-      if (customTime) {
-        time = parseInt(customTime);
+      if (!hasPathname || (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)) {
+        var target = $(this.hash);
+        if (target.length === 0) {
+          target =  $($(evt.currentTarget).attr("data-target"));
+        }
+
+        var time = 2000;
+        var offset = 10;
+        var customTime = $(evt.currentTarget).attr("data-scroll-time");
+        var customOffset = $(evt.currentTarget).attr("data-scroll-offset");
+
+        if (customTime) {
+          time = parseInt(customTime);
+        }
+
+        if (customOffset) {
+          offset = parseInt(customOffset);
+        }
+
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - offset
+          }, time);
+
+          // show hash in url without page jump
+          history.pushState(null, null, this.hash);
+          return false;
+        }
       }
-
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, time);
-
-        // show hash in url without page jump
-        history.pushState(null, null, this.hash);
-        return false;
-      }
-    }
   });
 });
